43f83b79a60e520d5f9a7dd913b524b255408a9c
semplicita <118811222+semplicita@users.noreply.github.com>
2022-11-24 11:46:32 +0200

    Add a function for playing a round

--- game.js
+++ game.js
@@ -11,3 +11,38 @@ function getComputerChoice() {
             return "Scissors"
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase()
+    computerSelection = computerSelection.toUpperCase()
+
+    let result
+
+    if (playerSelection === "ROCK") {
+        if (computerSelection === "PAPER") {
+            result = "You Lose! Paper beats Rock"
+        } else if (computerSelection === "SCISSORS") {
+            result = "You Win! Rock beats Scissors"
+        } else {
+            result = "It's draw!"
+        }
+    } else if (playerSelection === "PAPER") {
+        if (computerSelection === "SCISSORS") {
+            result = "You Lose! Scissors beats Paper"
+        } else if (computerSelection === "ROCK") {
+            result = "You Win! Paper beats Rock"
+        } else {
+            result = "It's draw!"
+        }
+    } else {
+        if (computerSelection === "ROCK") {
+            result = "You Lose! Rock beats Scissors"
+        } else if (computerSelection === "PAPER") {
+            result = "You Win! Scissors beats Paper"
+        } else {
+            result = "It's draw!"
+        }
+    }
+
+    return result
+  }
