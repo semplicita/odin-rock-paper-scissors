4315ce716cbf36cdeb3b00e12222803de18dfe0a
semplicita <118811222+semplicita@users.noreply.github.com>
2022-11-24 20:19:56 +0200

    Create user interface

--- game.js
+++ game.js
@@ -12,6 +12,9 @@ function getComputerChoice() {
     }
 }
 
+let playerScore = 0
+let computerScore = 0
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase()
     computerSelection = computerSelection.toUpperCase()
@@ -21,32 +24,77 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "ROCK") {
         if (computerSelection === "PAPER") {
             result = "You Lose! Paper beats Rock"
+            computerScore++
         } else if (computerSelection === "SCISSORS") {
             result = "You Win! Rock beats Scissors"
+            playerScore++
         } else {
             result = "It's draw!"
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "SCISSORS") {
             result = "You Lose! Scissors beats Paper"
+            computerScore++
         } else if (computerSelection === "ROCK") {
             result = "You Win! Paper beats Rock"
+            playerScore++
         } else {
             result = "It's draw!"
         }
     } else {
         if (computerSelection === "ROCK") {
             result = "You Lose! Rock beats Scissors"
+            computerScore++
         } else if (computerSelection === "PAPER") {
             result = "You Win! Scissors beats Paper"
+            playerScore++
         } else {
             result = "It's draw!"
         }
     }
 
-    return result
+    let finalResult
+
+    if (playerScore === 5) {
+        finalResult = "Final Winner: YOU!"
+    } else if (computerScore === 5) {
+        finalResult = "Final Winner: The Computer!"
+    }
+
+    updateScore(result, finalResult, playerScore, computerScore)
+
   }
 
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice())
+    })
+})
+
+const container = document.querySelector('#container')
+const content = document.createElement('div')
+const playerCurrentScore = document.createElement('div')
+const computerCurrentScore = document.createElement('div')
+const finalResultString = document.createElement('div')
+
+container.appendChild(content)
+container.appendChild(playerCurrentScore)
+container.appendChild(computerCurrentScore)
+container.appendChild(finalResultString)
+
+function updateScore(result, finalResult, playerScore, computerScore) {
+    content.textContent = result
+    playerCurrentScore.textContent = `Your score: ${playerScore}`
+    computerCurrentScore.textContent = `Computer's score: ${computerScore}`
+    finalResultString.textContent = finalResult
+}
+
+
+  
+
+  /*
   function game() {
 
     let playerWins = 0
@@ -75,3 +123,4 @@ function playRound(playerSelection, computerSelection) {
   }
 
   game()
+  */
